d4d97b1d2d5725c12197f9ca0753d1e46bb0c325
dllewellyn <danieln.llewellyn@gmail.com>
2022-11-17 20:22:44 +0000

    .. Just adding temporarily

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 import Home from './views/Home';
 
 // Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+ReactGA.initialize("G-SB92XH48PM");
 
 const trackPage = page => {
   ReactGA.set({ page });
